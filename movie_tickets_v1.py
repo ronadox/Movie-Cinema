bfa98fab36ccf89b7d874d0c3f0d4f8692f0eb8b
ronadox <togiasoa2@middleton.school.nz>
2024-03-14 09:32:38 +1300

    Create movie_tickets_v1.py

---
+++ movie_tickets_v1.py
@@ -0,0 +1,20 @@
+"""Movie theatre ticketing system - v1
+Welcome screen and initialise variables
+"""
+
+def sell_ticket():
+    """Component 1 of project: Welcome screen and set up variables"""
+
+    print("*********** Arona's Theatre - ticketing system ***********")
+
+    print("Welcome to Arona's Theatre!")
+
+    adult_tickerts = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_tickets = 0
+    tickets_sold = 0
+    total_sales = 0
+
+# Main routine
+print(sell_ticket())
